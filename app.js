b0956e5e1740a71c59844406a51c58960133d0d5
Ben Warwick-Champion <ben.w-c@sky.com>
2021-03-04 21:13:58 +0000

    Add greyscale and black drawing and erasing

--- app.js
+++ app.js
@@ -16,4 +16,40 @@ function createGrid(size) {
     } );
 }
 
-createGrid(10);
+function initSquares() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            modeGreyscale(square);
+        });
+    });
+}
+
+function modeBlack(square) {
+    square.style.cssText = "background-color: #000";
+}
+
+function modeErase(square) {
+    square.style.cssText = "background-color: #fff";
+}
+
+function modeGreyscale(square) {
+    let currColour = square.style.backgroundColor;
+    if (currColour) {
+        square.style.cssText = `background-color: ${incGreyscale(currColour)};`;
+    } else {
+        square.style.cssText = "background-color: #ddd;";
+    };
+}
+
+function incGreyscale(rgb) {
+    let [r, g, b] = rgb.slice(4, -1).split(', ');
+    if (r-32 < 0) {
+        return 'rgb(0, 0, 0)'
+    }
+    return `rgb(${r-32}, ${g-32}, ${b-32})`;
+}
+
+createGrid(40);
+initSquares();
+console.log((parseInt('d', 16) - 2).toString(16));
